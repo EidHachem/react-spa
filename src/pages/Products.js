f11ef8b83696aeaa08d19ddfa4ef0fd09ecd9438
eid <eidhachem1@gmail.com>
2024-09-19 11:21:39 +0300

    Add single product page with route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,24 @@
 import React from "react"
 
+import { Link } from "react-router-dom"
+
 const Products = () => {
-  return <h1>Products Page</h1>
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Pen</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Kindle</Link>
+        </li>
+      </ul>
+    </section>
+  )
 }
 
 export default Products
